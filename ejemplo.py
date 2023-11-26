c6150747358b83171e74bbaa6abfe4068061272d
raqu1 <127392380+Raqu1@users.noreply.github.com>
2023-11-26 17:44:32 +0000

    Tercer ejercicio

---
+++ ejemplo.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+hola1 = "Hola son las",6,"de la tarde"
+print(hola1)
+type(hola1)
+hola2 = "Hola son las "+str(6)+" de la tarde"
+print(hola2)
+type(hola2)
---
+++ programa3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import math
+cateto1 = float(input("Introduzca la medida del cateto 1: "))
+cateto2 = float(input("I∑ntroduzca la medida del cateto 2: "))
+hipotenusa = math.sqrt(cateto1 ** 2 + cateto2 **2)
+print("La hipotenusa es" ,hipotenusa)
