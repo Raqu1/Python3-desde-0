e4514a307d4c87b37926a46473294c2162121546
raqu1 <127392380+Raqu1@users.noreply.github.com>
2023-11-27 21:55:36 +0000

    Estructuras de control alternativas 4

---
+++ alternativa4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+user = input("Introduce el usuario: ")
+passwd = input("Introduce el password: ")
+if user == "pepe" and passwd == "asdasd":
+	print("Has entrado al sistema")
+else:
+	print("Usuario/Password incorrectos")
