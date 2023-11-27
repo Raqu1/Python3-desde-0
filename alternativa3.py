3df09cf63893230d71dd6226764087ad2cb9fd44
raqu1 <127392380+Raqu1@users.noreply.github.com>
2023-11-27 21:50:35 +0000

    Estructuras de control alternativas 3

---
+++ alternativa3.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+num = int(input("Dime tu numero: "))
+if num == 0:
+	print("Es igual a 0")
+elif num > 0:
+	print("Es positivo")
+else:
+	print("Es negativo")
+print("Programa Terminado")
