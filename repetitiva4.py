c4914adb646de534bf79562dc0e3ef06887a5b5e
raqu1 <127392380+Raqu1@users.noreply.github.com>
2023-11-29 21:01:20 +0000

    Estructuras de control repetitivas: while (4)

---
+++ repetitiva4.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+secreto = "asdasd"
+while True:
+	clave = input("Dime la clave: ")
+	if clave != secreto:
+		print("Clave incorrecta")
+	if clave == secreto:
+		break;
+print("Bienvenido!!")
+
